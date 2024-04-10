2c39eeba30bd0f614b2829c314d052022cbd6a71
itaintkevin <kpjtofficial@gmail.com>
2024-04-10 08:55:48 +0530

    feat: added auth routes, updated connect utils function

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { addNFTData, getNFTData } from "./controllers/nfts.controller";
 import { createEvent, getAllEvents } from "./controllers/event.controller";
+import { register, login } from "./controllers/user.controller";
+import { authenticateToken } from "./middlewares/auth.middleware";
 
 const router = Router();
 
-router.get("/api/nfts", getNFTData);
-router.post("/api/nfts", addNFTData);
-router.get("/api/events", getAllEvents);
-router.post("/api/events", createEvent);
+router.post("/api/auth/register", register);
+router.post("/api/auth/login", login);
+
+router.get("/api/nfts", authenticateToken, getNFTData);
+router.post("/api/nfts", authenticateToken, addNFTData);
+router.get("/api/events", authenticateToken, getAllEvents);
+router.post("/api/events/create", authenticateToken, createEvent);
 
 export default router;
