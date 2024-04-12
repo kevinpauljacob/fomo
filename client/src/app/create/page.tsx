2af263d62325a79afabda1bb6b996f5a53015f63
itaintkevin <kpjtofficial@gmail.com>
2024-04-12 16:04:33 +0530

    feat: uploading file to arweave before create request

--- client/src/app/create/page.tsx
+++ client/src/app/create/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useRouter } from "next/navigation";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
@@ -16,6 +16,9 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { toast } from "react-toastify";
+import { UserContext } from "@/context/userContext";
+import axios from "axios";
+import { toMetaplexFile } from "@metaplex-foundation/js";
 
 interface EventData {
   eventName: string;
@@ -25,15 +28,13 @@ interface EventData {
   startDate: Date;
   endDate: Date;
   location: string;
-  capacity: number;
-  approval: boolean;
-  ticketId: string;
-  ticketPrice: number;
-  nftIpfsUri: string;
-  picture: File | null;
+  capacity: string;
+  ticketPrice: string;
+  imgUri: string;
 }
 
 export default function CreateEvent() {
+  const { user, metaplex } = useContext(UserContext);
   const router = useRouter();
   const [formData, setFormData] = useState<EventData>({
     eventName: "",
@@ -43,13 +44,17 @@ export default function CreateEvent() {
     startDate: new Date(),
     endDate: new Date(),
     location: "",
-    capacity: 0,
-    approval: false,
-    ticketId: "",
-    ticketPrice: 0,
-    nftIpfsUri: "",
-    picture: null,
+    capacity: "0",
+    ticketPrice: "0.0",
+    imgUri: "",
   });
+  const [file, setFile] = useState<File | null>(null);
+
+  useEffect(() => {
+    if (!user || !user.isLoggedIn) {
+      router.replace("/auth/signin");
+    }
+  }, [router, user]);
 
   const handleInputChange = (
     e: React.ChangeEvent<
@@ -74,42 +79,73 @@ export default function CreateEvent() {
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
-    setFormData((prevData) => ({
-      ...prevData,
-      picture: file || null,
-    }));
+    console.log("Selected file:", file); // Log the selected file to check if it's captured
+    setFile(file || null);
   };
 
-  const handleSwitchChange = (checked: boolean) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      approval: checked,
-    }));
-  };
+  async function uploadImage(file: File | null) {
+    if (!file) {
+      console.error("File is null");
+      return;
+    }
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = async (event: any) => {
+        try {
+          const imgBuffer = new Uint8Array(event.target.result);
+          const imgMetaplexFile = toMetaplexFile(imgBuffer, file.name);
+
+          const imgUri = await metaplex.storage().upload(imgMetaplexFile);
+          setFormData((prevData) => ({
+            ...prevData,
+            imgUri: imgUri,
+          }));
+          console.log("Image URI:", imgUri);
+          toast.success("Image Uploaded to Arweave", {
+            position: "bottom-right",
+          });
+          resolve(imgUri);
+        } catch (error) {
+          console.error("Error uploading image:", error);
+          toast.error("Error uploading image", {
+            position: "bottom-right",
+          });
+          reject(error);
+        }
+      };
+
+      reader.readAsArrayBuffer(file);
+    });
+  }
+
+  useEffect(() => {
+    uploadImage(file);
+  }, [file]);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    console.log("Form data:", formData);
 
     try {
-      const response = await fetch("/api/events/create", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+      console.log("updated form data", formData);
+      const response = await axios.post(
+        "http://localhost:3333/api/events/create",
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
 
-      if (!response.ok) {
-        throw new Error("Failed to create event");
-      }
-
-      const responseData = await response.json();
-      console.log(responseData);
-      toast.success("Event created successfully", { position: "top-right" });
-      router.push("/events");
+      console.log(response.data);
+      toast.success("Event created successfully", { position: "bottom-right" });
+      router.push("/explore");
     } catch (error) {
       console.error("Error creating event:", error);
-      toast.error("Failed to create event", { position: "top-right" });
+      toast.error("Failed to create event", { position: "bottom-right" });
     }
   };
 
@@ -123,9 +159,9 @@ export default function CreateEvent() {
           <div className="flex justify-between">
             <div className="w-[38%]">
               <div className="w-[323px] h-[323px] relative">
-                {formData.picture && (
+                {file && (
                   <img
-                    src={URL.createObjectURL(formData.picture)}
+                    src={URL.createObjectURL(file)}
                     alt="Preview"
                     style={{
                       width: "100%",
@@ -153,125 +189,40 @@ export default function CreateEvent() {
                   </label>
                 </div>
               </div>
-              <p className="text-white/70 font-bold mt-5 mb-1">Event Options</p>
+              <p className="text-white/70 font-bold mt-4 mb-1">Event Options</p>
               <div>
-                <div className="flex justify-between bg-black/20 autofill-none outline-none border border-white/20 rounded-lg px-3 py-1.5 mt-1 h-2/3">
-                  <Label className="flex justify-center items-center">
-                    Ticket Price
-                  </Label>
-                  <div className="flex gap-2 justify-center items-center">
-                    <Dialog>
-                      <DialogTrigger asChild className="bg-black/20 h-5/6">
-                        <Button
-                          variant="outline"
-                          className="gap-2 border-none bg-transparent hover:bg-transparent p-1"
-                        >
-                          <p className="text-white/50">Edit</p>
-                          <Image
-                            src={"/pencilo.svg"}
-                            alt="edit"
-                            width={15}
-                            height={15}
-                            className="cursor-pointer"
-                          />
-                        </Button>
-                      </DialogTrigger>
-                      <DialogContent className="sm:max-w-[425px] bg-gray-950 border-none">
-                        <DialogHeader>
-                          <DialogTitle>Edit Price</DialogTitle>
-                          <DialogDescription>
-                            Make changes to your Price here. Click save when
-                            you're done.
-                          </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                          <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="name" className="text-right">
-                              Price
-                            </Label>
-                            <Input
-                              id="name"
-                              defaultValue=""
-                              className="col-span-3 bg-black/20 autofill-none outline-none border border-white/20 focus:border-white/50 focus:bg-black/30 rounded-lg px-3 py-1.5 mt-1 placeholder:text-sm"
-                            />
-                          </div>
-                        </div>
-                        <DialogFooter>
-                          <Button type="submit">Save changes</Button>
-                        </DialogFooter>
-                      </DialogContent>
-                    </Dialog>
-                  </div>
+                <div className="flex flex-col my-3">
+                  <Label>Ticket Price</Label>
+                  <input
+                    type="text"
+                    name="ticketPrice" // The name attribute should match the key in the state object
+                    id="ticket-price"
+                    className="bg-black/20 autofill-none outline-none border border-white/20 focus:border-white/50 focus:bg-black/30 rounded-lg px-3 py-1.5 mt-1 placeholder:text-sm"
+                    placeholder="0.0"
+                    value={formData.ticketPrice} // Use formData.description to set the value
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
-                <div className="flex justify-between bg-black/20 autofill-none outline-none border border-white/20 rounded-lg px-3 py-1.5 mt-1">
-                  <Label className="flex justify-center items-center">
-                    Required
-                  </Label>
-                  <div className="flex justify-center items-center">
-                    <label className="relative inline-flex items-center cursor-pointer">
-                      <input
-                        type="checkbox"
-                        value=""
-                        className="sr-only peer"
-                      />
-                      <div className="group peer ring-0 bg-rose-400  rounded-full outline-none duration-300 after:duration-300 w-11 h-6 shadow-md peer-checked:bg-emerald-500  peer-focus:outline-none  after:rounded-full after:absolute after:bg-gray-50 after:outline-none after:h-4 after:w-4 after:top-1 after:left-1 after:flex after:justify-center after:items-center peer-checked:after:translate-x-5  peer-hover:after:scale-95"></div>
-                    </label>
-                  </div>
-                </div>
-                <div className="flex justify-between bg-black/20 autofill-none outline-none border border-white/20 rounded-lg px-3 py-1.5 mt-1">
-                  <Label className="flex justify-center items-center">
-                    Capacity
-                  </Label>
-                  <div className="flex gap-2 justify-center items-center">
-                    <Dialog>
-                      <DialogTrigger asChild className="bg-black/20 h-5/6">
-                        <Button
-                          variant="outline"
-                          className="gap-2 border-none bg-transparent hover:bg-transparent p-1"
-                        >
-                          <p className="text-white/50">Unlimited</p>
-                          <Image
-                            src={"/pencilo.svg"}
-                            alt="edit"
-                            width={15}
-                            height={15}
-                            className="cursor-pointer"
-                          />
-                        </Button>
-                      </DialogTrigger>
-                      <DialogContent className="sm:max-w-[425px] bg-gray-950 border-none">
-                        <DialogHeader>
-                          <DialogTitle>Edit Capacity</DialogTitle>
-                          <DialogDescription>
-                            Make changes to your Event Capacity here. Click save
-                            when you're done.
-                          </DialogDescription>
-                        </DialogHeader>
-                        <div className="grid gap-4 py-4">
-                          <div className="grid grid-cols-4 items-center gap-4">
-                            <Label htmlFor="name" className="text-right">
-                              Capacity
-                            </Label>
-                            <Input
-                              id="name"
-                              defaultValue="0"
-                              className="col-span-3 bg-black/20 autofill-none outline-none border border-white/20 focus:border-white/50 focus:bg-black/30 rounded-lg px-3 py-1.5 mt-1 placeholder:text-sm"
-                            />
-                          </div>
-                        </div>
-                        <DialogFooter>
-                          <Button type="submit">Save changes</Button>
-                        </DialogFooter>
-                      </DialogContent>
-                    </Dialog>
-                  </div>
+                <div className="flex flex-col my-3">
+                  <Label>Capacity</Label>
+                  <input
+                    type="text"
+                    name="capacity" // The name attribute should match the key in the state object
+                    id="capacity"
+                    className="bg-black/20 autofill-none outline-none border border-white/20 focus:border-white/50 focus:bg-black/30 rounded-lg px-3 py-1.5 mt-1 placeholder:text-sm"
+                    placeholder="0"
+                    value={formData.capacity} // Use formData.description to set the value
+                    onChange={handleInputChange}
+                    required
+                  />
                 </div>
               </div>
             </div>
             <div className="w-[58%]">
               <div className="pb-1">
-                <Input
-                  className="bg-transparent outline-none border-none text-4xl font-bold text-white placeholder:text-white/50 px-0"
+                <input
+                  className="bg-transparent outline-none ring-0 focus:ring-0 border-none text-4xl font-bold text-white placeholder:text-white/50 px-0"
                   type="text"
                   name="eventName"
                   placeholder="Event Name"
@@ -284,10 +235,12 @@ export default function CreateEvent() {
                 <Label>Description</Label>
                 <input
                   type="text"
-                  name="price"
-                  id="price"
+                  name="description" // The name attribute should match the key in the state object
+                  id="description"
                   className="bg-black/20 autofill-none outline-none border border-white/20 focus:border-white/50 focus:bg-black/30 rounded-lg px-3 py-1.5 mt-1 placeholder:text-sm"
-                  placeholder="description..."
+                  placeholder="Description"
+                  value={formData.description} // Use formData.description to set the value
+                  onChange={handleInputChange}
                   required
                 />
               </div>
