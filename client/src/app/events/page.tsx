c98ba2bc79655e8261dd30421c801903fc6d8e72
itaintkevin <kpjtofficial@gmail.com>
2024-04-12 16:05:15 +0530

    feat: events route integrated

--- client/src/app/events/page.tsx
+++ client/src/app/events/page.tsx
@@ -1,38 +1,82 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import YourEventsCard from "@/components/explore/YourEventsCard";
-interface Event {
-  src: string;
-  href: string;
-  title: string;
-  // hostedBy: string;
-  // startDate: string;
-  // endDate: string;
-  // location: string;
-  // ticketPrice: number;
-  // capacity: number;
-  description: string;
-  // eventId: string;
-  // organizerWalletAddress: string;
+import { UserContext } from "@/context/userContext";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface Ticket {
+  eventId: string;
+  ticketId: string;
+  nftAddress: string;
+  walletAddress: string;
 }
 
+interface Event {
+  eventName: string;
+  organizer: string;
+  organizerWalletAddress: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  location: string;
+  capacity: string;
+  ticketPrice: string;
+  eventId: string;
+  imgUri: string;
+}
 export default function page() {
-  const [yourEvents, setYourEvents] = useState<Event[]>([
-    {
-      src: "/assets/image1.avif",
-      href: "/link/destination1",
-      title: "Music Festival Spectacular",
-      description:
-        "Experience the ultimate music festival with top artists performing live, food stalls, and exciting activities for all music enthusiasts.",
-    },
-    {
-      src: "/assets/image2.avif",
-      href: "/link/destination2",
-      title: "Adventure Trekking Expedition",
-      description:
-        "Embark on an adventurous trekking expedition through breathtaking landscapes, challenging terrains, and hidden trails.",
-    },
-  ]);
+  const { user } = useContext(UserContext);
+  const router = useRouter();
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+  const [events, setEvents] = useState<Event[]>([]);
+
+  useEffect(() => {
+    if (!user || !user.isLoggedIn) {
+      router.replace("/auth/signin");
+    }
+  }, [router, user]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      try {
+        const response = await axios.get("http://localhost:3333/api/tickets", {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+          params: { walletAddress: user.walletAddress },
+        });
+        setTickets(response.data.tickets);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    };
+
+    if (user && user.token !== "" && user.walletAddress !== "") {
+      fetchTickets();
+    }
+  }, [user]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const eventIds = tickets.map((ticket) => ticket.eventId);
+        const response = await axios.get("http://localhost:3333/api/events", {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+          params: { eventIds: eventIds },
+        });
+        setEvents(response.data.events);
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    };
+
+    if (tickets.length > 0) {
+      fetchEvents();
+    }
+  }, [tickets]);
 
   return (
     <div className="min-h-[calc(100vh-161px)] max-h-full max-w-[768px] mx-auto mt-7">
@@ -43,22 +87,20 @@ export default function page() {
           Explore unique experiences and create unforgettable memories today.
         </p>
       </div>
-      {yourEvents && yourEvents.length !== 0 && (
-        <div className="mt-5 mb-7">
-          <h2 className="text-lg font-semibold mb-2">Your Events</h2>
-          <div className="">
-            {yourEvents.map((data, index) => (
-              <YourEventsCard
-                key={index}
-                src={data.src}
-                href={data.href}
-                title={data.title}
-                description={data.description}
-              />
-            ))}
+      <div className="grid grid-cols-3 gap-4">
+        {events && events.length !== 0 ? (
+          <div className="mt-5 mb-7">
+            <h2 className="text-lg font-semibold mb-2">Your Events</h2>
+            <div className="">
+              {events.map((event, index) => (
+                <YourEventsCard key={index} event={event} />
+              ))}
+            </div>
           </div>
-        </div>
-      )}
+        ) : (
+          <p>You have not attended any events.</p>
+        )}
+      </div>
     </div>
   );
 }
