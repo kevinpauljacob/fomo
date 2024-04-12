60eeca131100265e55f9e9136f133277e95c129b
itaintkevin <kpjtofficial@gmail.com>
2024-04-12 16:01:07 +0530

    feat: updated createEvent controller and created getEventsByIds controller

--- server/src/controllers/event.controller.ts
+++ server/src/controllers/event.controller.ts
@@ -1,18 +1,18 @@
 import { Request, Response } from "express";
 import EventModel, { Event } from "../models/event.model"; // Assuming your model file is named Event.ts
+import { v4 as uuidv4 } from "uuid";
 
 export const createEvent = async (
   req: Request,
   res: Response
 ): Promise<void> => {
   try {
-    // Extract event details from request body
+    const eventId: string = uuidv4();
     const eventData: Event = req.body;
-
-    // Create a new event document
-    const newEvent: Event = new EventModel(eventData);
-
-    // Save the new event to the database
+    const newEvent: Event = new EventModel({
+      ...eventData,
+      eventId: eventId,
+    });
     await newEvent.save();
 
     res
@@ -35,6 +35,38 @@ export const getAllEvents = async (
     res.status(200).json({ events });
   } catch (error) {
     console.error("Error fetching events:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getEventsByIds = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { eventIds }: { eventIds?: string[] } = req.query;
+
+    if (!eventIds || !Array.isArray(eventIds) || eventIds.length === 0) {
+      res.status(400).json({
+        error: "eventIds must be a non-empty array in the request body",
+      });
+      return;
+    }
+
+    const events: Event[] = await EventModel.find({
+      eventId: { $in: eventIds },
+    });
+
+    if (events.length === 0) {
+      res
+        .status(404)
+        .json({ error: "No events found for the specified eventIds" });
+      return;
+    }
+
+    res.status(200).json({ events });
+  } catch (error) {
+    console.error("Error fetching events by eventIds:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
